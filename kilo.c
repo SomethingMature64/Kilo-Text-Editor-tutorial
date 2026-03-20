fff09c978cad5634f88ae13aa455426a006ca9ed
SomethingMature64 <jojogana0@outlook.com>
2026-03-20 08:38:25 +0100

    More like an editor now, I don't know what to put here

--- kilo.c
+++ kilo.c
@@ -1,43 +1,136 @@
 #include <windows.h>
 #include <stdio.h> //What does this library do?
+#include <stdlib.h>
+#include <ctype.h>
 
-DWORD originalMode;
-HANDLE hInput;
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+struct editorConfig{
+    int cx, cy;
+    int screenrows;
+    int screencols;
+
+    HANDLE hInput;
+    HANDLE hOutput;
+
+    DWORD originalMode;
+};
+
+struct editorConfig E;
 
 void disableRawMode()
 {
-    SetConsoleMode(hInput,originalMode);
+    SetConsoleMode(E.hInput,E.originalMode); //Why did we switch to a struct?
 }
 
 void enableRawMode()
 {
-    hInput = GetStdHandle(STD_INPUT_HANDLE);
+    E.hInput = GetStdHandle(STD_INPUT_HANDLE);
+    E.hOutput = GetStdHandle(STD_OUTPUT_HANDLE); //What do these get?
     
 
-    GetConsoleMode(hInput, &originalMode);
-    atexit(disableRawMode);
+    GetConsoleMode(E.hInput, &E.originalMode);
 
-    DWORD raw = originalMode;
+    DWORD raw = E.originalMode;
 
     raw &= ~(ENABLE_ECHO_INPUT |
              ENABLE_LINE_INPUT |
              ENABLE_PROCESSED_INPUT); 
 
-    SetConsoleMode(hInput,raw);
+    SetConsoleMode(E.hInput,raw);
+    atexit(disableRawMode); //Does the order of this function's placement matter?
+}
+
+int editorReadKey(){
+    INPUT_RECORD record; //? What's this
+    DWORD read;
+
+    while(1)
+    {   //? What is going on here?
+        ReadConsoleInput(E.hInput,&record,1,&read);
+
+        if (record.EventType == KEY_EVENT &&
+            record.Event.KeyEvent.bKeyDown)
+        {
+            return record.Event.KeyEvent.uChar.AsciiChar; 
+        }
+        
+    }
+}
+
+void editorProcessKeypress()
+{
+    int c = editorReadKey();
+    if (c==CTRL_KEY('q'))
+    {
+        exit(0);
+    }
+
+    if (iscntrl(c))
+    {
+        printf("%d\n",c);
+    } else
+    {
+        printf("%d ('%c')\n",c,c);
+    }
+}
+
+void editorClearScreen()
+{
+    printf("\x1b[2j");
+    printf("\x1b[H");
+}
+
+void editorDrawRows()
+{
+    for (int y = 0; y < E.screenrows; y++)
+    {
+        printf("~\r\n");
+    }
+}
+
+void editorRefreshScreen()
+{
+    editorClearScreen();
+    editorDrawRows();
+    printf("\x1b[H");
+}
+
+int getWindowSize(int *rows, int *cols)
+{
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    
+    if (!GetConsoleScreenBufferInfo(E.hOutput,&csbi))
+    {
+        return -1;
+    }
+    
+    *cols = csbi.srWindow.Right - csbi.srWindow.Left +1;
+    *rows = csbi.srWindow.Bottom - csbi.srWindow.Top +1;
+
+    return 0;
+}
+
+void initEditor()
+{
+    E.cx = 0;
+    E.cy = 0;
+
+    if (getWindowSize(&E.screenrows, &E.screencols)==-1)
+    {
+        exit(1);
+    }
 }
 
 int main()
 {
     enableRawMode();
+    initEditor();
 
-    char c;
-    DWORD read;
-
-    HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
-
-    while (ReadConsoleA(hInput,&c,1,&read,NULL)&&c!='q') //? What is this syntax?
+    while (1)
     {
-        printf("%d ('%c')\n",c,c);
+        editorRefreshScreen();
+        editorProcessKeypress();
     }
     
     return 0;
